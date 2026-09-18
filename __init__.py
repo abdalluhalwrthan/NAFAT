39f8f4e6fdd7b43155b312e0d0d000b79223c0c8
abdalluhalwrthan <abdalluhalwrthan@gmail.com>
2026-09-19 02:43:18 +0300

    v1.0

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # NAFAT Package Initialization
 __app_name__ = "NAFAT"
-__version__ = "1.0.0"
+__version__ = "1.0"
 __description__ = "Network Authentication Feasibility Assessment Tool"
 __author__ = "Abdullah Alwarthan"
